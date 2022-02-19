f10e45668271e3d671a8d5f8a9efd96ef900edd3
Ilya Arefiev <arefiev.id@gmail.com>
2022-02-19 17:22:33 +0300

    add/delete items

--- main.js
+++ main.js
@@ -1,5 +1,55 @@
+/* -*- mode: js; js-indent-level: 2 -*- */
+
 document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-    console.log('main');
+  console.log('main');
+
+  let appContainer = document.querySelector('.application');
+  dishAdd(appContainer);
+}
+
+function _addTemplateItem(target, selector) {
+  let template = document.querySelector('.template ' + selector).cloneNode(true);
+
+  let node;
+  if (!!target.firstChild) {
+    node = target.insertBefore(template, target.firstChild);
+  } else {
+    node = target.appendChild(template);
+  }
+
+  return node;
+}
+
+function _deleteItem(node, selector) {
+  let parent = node.parentNode;
+  let childs = parent.querySelectorAll(selector);
+
+  if (childs.length > 1) {
+    parent.removeChild(node);
+  }
+}
+
+function _addBtnHandler(target, selector, handler) {
+  target.querySelector(selector).addEventListener('click', handler);
+}
+
+function dishAdd(target) {
+  let dish = _addTemplateItem(target, '.dish');
+
+  _addBtnHandler(dish, '.dish .add', () => dishAdd(target));
+  _addBtnHandler(dish, '.dish .delete', () => _deleteItem(dish, '.dish'));
+
+  let ingredientsContainer = dish.querySelector('.ingredients');
+
+  _addBtnHandler(dish, '.ingredient .add', () => ingredientAdd(ingredientsContainer));
+  _addBtnHandler(dish, '.ingredient .delete', (ev) => _deleteItem(ev.target.parentNode.parentNode, '.ingredient'));
+}
+
+function ingredientAdd(target) {
+  let ingredient = _addTemplateItem(target, '.ingredient');
+
+  _addBtnHandler(ingredient, '.ingredient .add', () => ingredientAdd(target));
+  _addBtnHandler(ingredient, '.ingredient .delete', () => _deleteItem(ingredient, '.ingredient'));
 }
