c2a0cd0fe6847621c185ffd3ff6171188e288aab
Ilya Arefiev <arefiev.id@gmail.com>
2022-02-19 19:11:30 +0300

    compute portions

--- main.js
+++ main.js
@@ -41,10 +41,15 @@ function dishAdd(target) {
   _addBtnHandler(dish, '.dish .add', () => dishAdd(target));
   _addBtnHandler(dish, '.dish .delete', () => _deleteItem(dish, '.dish'));
 
+  dish.querySelector('.weight_row input[name=dish_outcome]').addEventListener('change', recompute);
+  dish.querySelector('.weight_row input[name=dish_portion]').addEventListener('change', recompute);
+
   let ingredientsContainer = dish.querySelector('.ingredients');
 
   _addBtnHandler(dish, '.ingredient .add', () => ingredientAdd(ingredientsContainer));
   _addBtnHandler(dish, '.ingredient .delete', (ev) => _deleteItem(ev.target.parentNode.parentNode, '.ingredient'));
+
+  dish.querySelector('.weight_row input[name=ingredient_weight]').addEventListener('change', recompute);
 }
 
 function ingredientAdd(target) {
@@ -52,4 +57,51 @@ function ingredientAdd(target) {
 
   _addBtnHandler(ingredient, '.ingredient .add', () => ingredientAdd(target));
   _addBtnHandler(ingredient, '.ingredient .delete', () => _deleteItem(ingredient, '.ingredient'));
+
+  ingredient.querySelector('.weight_row input[name=ingredient_weight]').addEventListener('change', recompute);
+}
+
+function _getInputValue(target, name) {
+  let value = target.querySelector('input[name=' + name + ']').value;
+  return parseInt(value) || 0;
+}
+
+function recompute(ev) {
+  let dish = ev.target;
+  let check = (n) => n.classList.contains('dish')
+
+  while (!check(dish)) {
+    if (!dish.parentNode) break;
+
+    dish = dish.parentNode;
+  }
+
+  if (!check(dish)) {
+    console.log('failed to find a dish');
+    return;
+  }
+
+  let ingredients = Array.from(dish.querySelectorAll('.ingredient'));
+
+  let total = ingredients.reduce(
+    (a, i) => a + _getInputValue(i, 'ingredient_weight'),
+    0
+  );
+
+  dish.querySelector('input[name=dish_total]').value = total;
+
+  let outcome = _getInputValue(dish, 'dish_outcome');
+  let portion = _getInputValue(dish, 'dish_portion');
+
+  let proceed = total > 0 && outcome > 0 && portion > 0;
+
+  if (!proceed) return;
+
+  let factor = portion / outcome;
+
+  ingredients.forEach((i) => {
+    let weight = _getInputValue(i, 'ingredient_weight');
+    let value = weight * factor;
+    i.querySelector('input[name=ingredient_portion]').value = value.toFixed(2);
+  });
 }
